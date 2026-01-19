5ca2d882791c83c9453ab4558b32fdce91dfd68e
mannu <mannukumar9504@gmail.com>
2026-01-19 15:12:40 +0530

    response message changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/',(req, res) => {
-    res.send("Hello World");
+    res.send("Hello World updated");
 })
 app.listen(80, ()=> {
     console.log("server running on port 80")
